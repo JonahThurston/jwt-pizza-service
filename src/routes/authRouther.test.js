c64b8b23b10e2bab4e4cecdc1189d2dedf76957f
Jonah Thurston <jonah.m.thurston@gmail.com>
2025-01-31 12:51:42 -0700

    make login test match docs

--- src/routes/authRouther.test.js
+++ src/routes/authRouther.test.js
@@ -8,18 +8,24 @@ beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
   const registerRes = await request(app).post("/api/auth").send(testUser);
   testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
 });
 
 test("login", async () => {
   const loginRes = await request(app).put("/api/auth").send(testUser);
   expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: "diner" }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(
     /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
   );
-
-  const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
-  expect(loginRes.body.user).toMatchObject(user);
-});
+}
 
 test("register", async () => {
   const user = { name: "pizza diner", email: "reg@test.com", password: "a" };
